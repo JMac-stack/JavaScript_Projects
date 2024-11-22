9d6f9b9c8be2304afad9e5d060a43b11670d2045
JMac-stack <jamesmclauchlan2@live.co.uk>
2024-11-22 12:00:53 +0000

    Update main.js

--- Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
+++ Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
@@ -1,3 +1,8 @@
-var x = "\"Hello, " + "world!\"", y = 1, z = 2;
-window.alert(x);
-document.write(x);
+var x = "\"Hello, " + "world!\"", y = 1, z = 2; //Escape character, Concatenation, Multiple assignment.
+window.alert(x); //Alert window with text "Hello, world!".
+document.write(x); //Prints "Hello, world!" to document.
+document.write("<br>" + y); //Makes new line on document and prints the value 1.
+10 * 2 //Expression.
+
+var Sent1 = "This is the beginning of the string", Sent2 = " and this is the end of the string.", FullSent = Sent1 + Sent2; //Required.
+document.write("<br>" + FullSent)
