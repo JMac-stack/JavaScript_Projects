92439d3e6d17eb6a5b2363f1bfb13d2fcc11d481
JMac-stack <jamesmclauchlan2@live.co.uk>
2024-11-22 12:57:26 +0000

    Finished Assignment

--- Basic-JavaScript-Projects/Project2_functions/JS/main.js
+++ Basic-JavaScript-Projects/Project2_functions/JS/main.js
@@ -1,4 +1,6 @@
-function My_First_Function() {
-    var str = "This is the button text!";
-    document.getElementById("Button_text").innerHTML = str;
+function My_First_Function() { //Function for button click.
+    var str = "Thanks for clicking the button.", str2 = "Click";
+    str2 += "ed"; //Concatenated using += operator.
+    document.getElementById("p1").innerHTML = str; //Changes paragraph text.
+    document.getElementById("Button_Text").innerHTML = str2; //Changes button text.
 }
