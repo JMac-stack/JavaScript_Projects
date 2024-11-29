58b8a4cef384cacf0e1fcc2da1c50c39bfe8ba25
JMac-stack <jamesmclauchlan2@live.co.uk>
2024-11-29 12:09:53 +0000

    Project 5 Progress

--- Basic-JavaScript-Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic-JavaScript-Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,2 +1,7 @@
-var x = 2E310, y = -2E310; //Variable with value of infinity and -infinty respectively.
-document.write(typeof("3" + 3) + `<br>${x}<br>${y}`)// Type coercion by concaternation with string and using typeof operator. Template string to display x and y values.
+document.write("<strong>Typeof/Coecion</strong><br>" + typeof("3" + 3)); //Type coercion by concaternation with string and using typeof operator.
+document.write(`<br><strong>Infinity</strong><br>${2E310}<br>${-2E310}`); //Template string to display infinity and -infinty respectively.
+document.write(`<br><strong>Boolean</strong><br>${1<2}<br>${1>2}`); //Displays boolean values true and false.
+console.log(5 * 3); //Maths operation output to console.
+console.log(1>2); //Display false in console.
+document.write(`<br><strong>Double Equal sign</strong><br>${10 == "10"}<br>${10 == 1}`) //Displays true/false values using double equal signs.
+document.write(`<br><strong>Triple Equal sign</strong><br>${10 === 10}<br>${10 === "9"}<br>${10 === "10"}<br>${10 === 1}`)//Displays true/false values using triple equal signs.
