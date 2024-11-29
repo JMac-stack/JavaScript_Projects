cd510fb31ea1de29f5d8a09084a6599c840bc65a
JMac-stack <jamesmclauchlan2@live.co.uk>
2024-11-29 11:12:41 +0000

    Progress

---
+++ Basic-JavaScript-Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,9 @@
+function my_Dictionary() {
+    var Car ={
+        Brand:"Ford",
+        Model:"Fiesta",
+        Colour:"Black",
+        Doors:3
+    };
+    document.getElementById("Dictionary").innerHTML = Car.Brand;
+}
