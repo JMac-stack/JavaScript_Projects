1662d074099aa2c47545e0f1723b701587489cd1
JMac-stack <jamesmclauchlan2@live.co.uk>
2024-11-29 12:19:07 +0000

    Complete Project 5

--- Basic-JavaScript-Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic-JavaScript-Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -5,3 +5,6 @@ console.log(5 * 3); //Maths operation output to console.
 console.log(1>2); //Display false in console.
 document.write(`<br><strong>Double Equal sign</strong><br>${10 == "10"}<br>${10 == 1}`) //Displays true/false values using double equal signs.
 document.write(`<br><strong>Triple Equal sign</strong><br>${10 === 10}<br>${10 === "9"}<br>${10 === "10"}<br>${10 === 1}`)//Displays true/false values using triple equal signs.
+document.write(`<br><strong>AND</strong><br>${10 > 1 && 1 < 10}<br>${10 > 1 && 1 > 10}`)//AND operator to display ture and false.
+document.write(`<br><strong>OR</strong><br>${10 > 1 || 1 < 10}<br>${10 > 11 || 1 > 10}`)//OR operator to display ture and false.
+document.write(`<br><strong>NOT</strong><br>${!(10 < 1)}<br>${!(10 > 1)}`)//NOT operator to display ture and false.
