92eeda1661e04c612d8adb0767f3927fd1494fa4
JMac-stack <jamesmclauchlan2@live.co.uk>
2024-11-29 11:25:20 +0000

    Progress

--- Basic-JavaScript-Projects/Project4_dictionaries/JS/main.js
+++ Basic-JavaScript-Projects/Project4_dictionaries/JS/main.js
@@ -1,9 +1,10 @@
-function my_Dictionary() {
-    var Car ={
+function my_Dictionary() { //Function to output value to paragraph.
+    var Car ={ //Dictionary declared and assigned.
         Brand:"Ford",
         Model:"Fiesta",
         Colour:"Black",
         Doors:3
     };
-    document.getElementById("Dictionary").innerHTML = Car.Brand;
+    delete Car.Brand; //Deletes KVP.
+    document.getElementById("Dictionary").innerHTML = Car.Brand; //Outputs to paragraph using DOM. Will be undefined as KVP has been deleted.
 }
