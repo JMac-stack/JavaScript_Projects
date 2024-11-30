649843da60ee080c114de808616797b6de98295d
JMac-stack <jamesmclauchlan2@live.co.uk>
2024-11-30 15:14:21 +0000

    Progress

--- Basic-JavaScript-Projects/Misc_Projects/JS/javaScript_1.js
+++ Basic-JavaScript-Projects/Misc_Projects/JS/javaScript_1.js
@@ -1,3 +1,4 @@
+// Colour picker function.
 function pickColour() {
     let colour = document.getElementById("textIn").value;
     let stringConcat = "You picked the colour ";
@@ -29,3 +30,31 @@ function pickColour() {
     }
     document.getElementById("textOut").innerHTML = textOut;
 }
+
+// Class change function.
+function changePFunction() {
+    let x = document.getElementsByClassName("p")
+    x[0].innerHTML = "This has changed.";
+    x[1].innerHTML = "This has also changed.";
+}
+
+// Canvas assignment.
+const c1 = document.getElementById("c1");
+const ctx = c1.getContext("2d");
+
+const grd = ctx.createLinearGradient(100,100,250,0);
+grd.addColorStop(0, "black");
+grd.addColorStop(1, "white");
+
+ctx.fillStyle = grd;
+ctx.fillRect(0, 0, 500, 250);
+
+
+ctx.fillStyle = "purple";
+ctx.fillRect(50, 50, 400, 150);
+
+ctx.beginPath();
+ctx.moveTo(0,0);
+ctx.lineTo(500,250);
+ctx.stroke();
+
